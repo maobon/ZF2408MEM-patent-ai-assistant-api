704618b3cf76666a9b13ebe9dd4476747b4ccd85
fuyunpeng <83266443+fuyunpeng0818@users.noreply.github.com>
2024-07-08 01:05:00 +0800

    init project

---
+++ swagger_server/controllers/default_controller.py
@@ -0,0 +1,15 @@
+import connexion
+import six
+
+from swagger_server import util
+
+
+def get_user():  # noqa: E501
+    """Get user
+
+     # noqa: E501
+
+
+    :rtype: None
+    """
+    return 'do some magic!'
